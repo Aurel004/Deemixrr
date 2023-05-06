4c5ae1eaaed4fa7558fc21d620224ee1d3498295
Aurel004 <aurelien.ruat@hotmail.fr>
2023-05-06 20:45:05 +0200

    Ajout du bouton pour forcer l'actualisation des playlists

---
+++ Deemixrr/Models/PlaylistUpdateViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Deemixrr.Data;
+
+namespace Deemixrr.Models
+{
+    public class PlaylistUpdateViewModel
+    {
+        public IList<Playlist> Playlists { get; set; }
+    }
+}
